0df0c3ef7be88d6442e4f0bb032de1ae8ac3d51d
Eugeny <eburdo@gmail.com>
2021-05-15 11:08:59 +0000

    initial commit for api

---
+++ api/src/test/java/com/abnamro/assessment/api/person/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnamro.assessment.api.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
